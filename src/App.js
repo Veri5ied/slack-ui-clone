5e140075ab61cc03fecf7449a38ec8651ee9d6e7
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-10 11:22:51 +0100

    slack ui setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+      <div className="App">
+        <h3 className="welcome">We go again</h3>
+      </div>
+  );
+}
+
+export default App;
