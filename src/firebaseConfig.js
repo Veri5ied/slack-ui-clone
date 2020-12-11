9471ac4044cd30990ea0a2f41a61241efe9ad0dc
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-11 13:03:29 +0100

    firebase env setup

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
-import firebase from "firebase"
+import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARiLsrFaT3VqoLp1eOHwuJ4w1iblBMBDQ",
-  authDomain: "slack-web-app-a719e.firebaseapp.com",
-  projectId: "slack-web-app-a719e",
-  storageBucket: "slack-web-app-a719e.appspot.com",
-  messagingSenderId: "90900856430",
-  appId: "1:90900856430:web:eaadfc4c8a2e8fb033f07f",
-  measurementId: "G-S8G2YR2NWP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
