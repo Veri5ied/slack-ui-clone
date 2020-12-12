c6d92ce0cb00fbffd64590dc95e7ee5278920dcd
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-12 03:27:05 +0100

    chat screen and useparams hook

---
+++ src/screens/chat-screens/Chat.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./Chat.css";
+
+function Chat() {
+  const { id } = useParams();
+  return <div></div>;
+}
+
+export default Chat;
