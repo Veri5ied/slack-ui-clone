030d3372f46f13bfa5d582fbfac1bdfdadd7286a
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-14 11:24:48 +0100

    react router

--- src/screens/layout/Layout.js
+++ src/screens/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function Layout() {
   return (
