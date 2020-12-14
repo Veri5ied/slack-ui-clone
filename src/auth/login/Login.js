6e88ec6620fd3ebc6a6db868c0fd584140cad25f
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-14 16:06:24 +0100

    login description

--- src/auth/login/Login.js
+++ src/auth/login/Login.js
@@ -9,6 +9,8 @@ function Login() {
           src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
           alt=""
         />
+        <h1>Sign in to Veri5ied's workspace</h1>
+        <p>veri5ied.slack.com</p>
       </div>
     </div>
   );
