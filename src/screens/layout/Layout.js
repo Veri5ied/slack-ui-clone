aa427a1512f6eeea1abd0fec9398cbca8a4a52dd
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-14 15:31:34 +0100

    routes

--- src/screens/layout/Layout.js
+++ src/screens/layout/Layout.js
@@ -16,6 +16,9 @@ function Layout() {
             <Route path="/room/:roomId">
               <Chat />
             </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
           </Switch>
         </div>
       </Router>
