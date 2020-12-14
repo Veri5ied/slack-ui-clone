b8c22142e4cfcb3265450dd2b6dbff39de695398
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-14 11:59:06 +0100

    chat screen vh

--- src/screens/chat-screens/Chat.js
+++ src/screens/chat-screens/Chat.js
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 import "./Chat.css";
 
 function Chat() {
-  const { id } = useParams();
+  const { roomId } = useParams();
   return (
-    <div className="chat__header">
+    <div className="chat">
       <h1>
         This is the chat Page
       </h1>
--- src/screens/layout/Layout.js
+++ src/screens/layout/Layout.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "../chat-screens/Chat";
+import "./Layout.css";
 
 function Layout() {
   return (
     <div className="layout">
-      <Header />
-      <div className="layout__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="layout__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome to the Chat Screen</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
