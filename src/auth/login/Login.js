e244575af8a5acad8a6033d18a8acc95fac0ea65
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-14 16:03:21 +0100

    slack original image

--- src/auth/login/Login.js
+++ src/auth/login/Login.js
@@ -6,7 +6,7 @@ function Login() {
     <div className="login">
       <div className="login__container">
         <img
-          src="https://i.pcmag.com/imagery/reviews/07td46ju7p61LVb0Qwc5VF-6..v_1569479844.jpg"
+          src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
           alt=""
         />
       </div>
