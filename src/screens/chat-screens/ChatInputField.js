d50f0dbc4c679f8083397709cffa7072839792e7
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-15 11:26:27 +0100

    chat field css

---
+++ src/screens/chat-screens/ChatInputField.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ChatInputField.css"
+
+function ChatInputField() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatInputField
