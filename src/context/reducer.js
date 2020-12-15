d76a2e136542de26273fdf1d2e44ed13f586ef3a
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-15 01:53:52 +0100

    user switch

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -5,3 +5,17 @@ export const initialState = {
 export const actionTypes = {
   SET_USER: "SET_USER",
 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
+      break;
+
+    default:
+      break;
+  }
+};
