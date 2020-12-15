1f688a6c2976bd7e48fd61a445e38f58420f929c
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-15 11:41:35 +0100

    channel name

--- src/screens/chat-screens/ChatInputField.js
+++ src/screens/chat-screens/ChatInputField.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import "./ChatInputField.css"
+import React from "react";
+import "./ChatInputField.css";
 
-function ChatInputField() {
-    return (
-        <div className="chatinputfield">
-            <form>
-            <input type="text" name="text" id="text"/>
-            </form>
-        </div>
-    )
+function ChatInputField({ channelName, channelId }) {
+  const handleSend = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="chatinputfield">
+      <form>
+        <input
+          type="text"
+          placeholder={`Message #${channelName?.toLowerCase()}`}
+        />
+        <button type="submit" onClick={handleSend}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default ChatInputField
+export default ChatInputField;
