49618a2520d4f5f1dcaa3777bc62fb85c9d17b06
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-15 01:56:20 +0100

    app wrapper

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import AppContextProvider from "./context/StoreContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
