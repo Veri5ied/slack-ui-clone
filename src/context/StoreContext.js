5cc5c289efbdb2a766868b0864f71a9629068cf6
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-15 01:49:18 +0100

    bug fix

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer, useContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 export const AppContext = createContext();
 
